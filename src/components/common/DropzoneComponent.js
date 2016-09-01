543d4c439ace5593c8b55db498c464456c23bde4
pratik <lazy.monk.13@gmail.com>
2016-08-31 21:23:32 -0700

    progress bar working

--- src/components/common/DropzoneComponent.js
+++ src/components/common/DropzoneComponent.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import Dropzone from 'react-dropzone';
+import firebase from 'firebase';
 
 class DropzoneComponent extends Component {
 	constructor(props) {
@@ -7,38 +8,81 @@ class DropzoneComponent extends Component {
 
 		this.state = {
 			preview: null,
-			testing: 'test'
+			testing: '50%'
 		}
 
 		this.onDrop = this.onDrop.bind(this);
+		this.savePicture = this.savePicture.bind(this);
 	}
 
 	onDrop(file) {
 		console.log ('file:', file)
 		this.setState({
+			file: file[0],
 			preview: file[0].preview
 		})
+	}
+
+	savePicture() {
+		console.log ('this.state.file:', this.state.file)
+		const storageRef = firebase.storage().ref(`listingImages/${this.state.file.name}`);
+		const task = storageRef.put(this.state.file);
+
+			task.on('state_changed', snap => {
+				const percentage = (snap.bytesTransferred / snap.totalBytes) * 100;
+					this.setState({
+						percentage: `${percentage.toString()}%`
+					})
+
+				}
+
+		)
+
 	}
 
 	render() {
 		console.log ('this.state:', this.state)
 		return (
 			<div>
+
+				<img src="https://firebasestorage.googleapis.com/v0/b/ebayredux-247f8.appspot.com/o/listingImages%2Fcat%20jump.gif?alt=media&token=ea505817-b465-4d1b-9ce3-e8c40b5d3cab"/>
+				<div className="progress">
+					<div className="progress-bar progress-bar-success progress-bar-striped" role="progressbar" aria-valuemin="0" aria-valuemax="100" style={{width: this.state.percentage}}>
+					</div>
+				</div>
+
 				<If condition={!this.state.preview}>
 					<Dropzone onDrop={this.onDrop}>
 						<div>
 							Click or drop a file here
 						</div>
 					</Dropzone>
+					<button onClick={() => this.setState({testing: '20%'})}>
+						update
+					</button>
 
 				</If>
 
+
+
 				<If condition={this.state.preview}>
 					<img src={this.state.preview} />
+					<button
+						type="button"
+						className="btn btn-primary"
+						onClick={this.savePicture}>
+						Save Picture
+					</button>
 				</If>
 
 			</div>
 		)
+	}
+}
+
+const styles = {
+	progress: {
+		width: "70%"
 	}
 }
 
