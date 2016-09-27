54cba0f067086d6268b8549748ab1132f2fe85c3
pratik <lazy.monk.13@gmail.com>
2016-09-26 23:49:23 -0700

    promise chain in front end

--- src/actions/shopActions.js
+++ src/actions/shopActions.js
@@ -1,25 +1,38 @@
 import axios from 'axios';
 import * as types from './actionTypes';
+import { getCurrentUserSuccess } from './userActions';
+
 
 export function getAllShops() {
 	return dispatch => {
 		return axios.get('/api/shops')
 			.then(res => dispatch(getAllShopsSuccess(res.data)))
 			.catch(err => console.log ('err:', err))
-	}
+	};
 }
 
 export function getAllShopsSuccess(shops) {
 	return {
 		type: types.GET_ALL_SHOPS_SUCCESS,
 		shops
-	}
+	};
+}
+
+export function getCurrentShopSuccess(shop) {
+	return {
+		type: types.GET_CURRENT_SHOP_SUCCESS,
+		shop
+	};
 }
 
 export function buyShopById(shopId, userId) {
 	return dispatch => {
 		return axios.put(`/api/shops/buyShopById/${shopId}/${userId}`)
-			.then(res => console.log ('res:', res))
+			.then(res => {
+				dispatch(getCurrentUserSuccess(res.data[0]))
+				return(res.data[1]);
+			})
+			.then(shop => dispatch(getCurrentShopSuccess(shop)))
 			.catch(err => console.log ('err:', err));
-	}
+	};
 }
